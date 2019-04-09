627c39f5003812f7de1b395fb62264e247c02b90
manuel-unverdorben <45029510+manuel-unverdorben@users.noreply.github.com>
2019-04-09 16:34:03 +0200

    Erstellen einer Spirale mit Path

--- Main.java
+++ Main.java
@@ -1,51 +1,35 @@
 package application;
-
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Arc;
-import javafx.scene.shape.ArcType;
-import javafx.scene.shape.Circle;
-import javafx.scene.shape.Ellipse;
-import javafx.scene.shape.Line;
-
+import javafx.scene.shape.LineTo;
+import javafx.scene.shape.MoveTo;
+import javafx.scene.shape.Path;
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,900,900);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-
-			double eckigkeit = 0.1; // je größer, desto eckiger
-			double feinheit = 0.1; // je kleiner, desto feiner
-			final int limit = 10000;
-			double startX = 0;
-			double startY = 0;
-			double endX = 450;
-			double endY = 450;
-
-			for (double i = 1; i < limit-1; i+=eckigkeit) {
-				startX = endX;
-				startY = endY;
-				endX = startX + (Math.sin(i)*(i)*feinheit);
-				endY = startY + (Math.cos(i)*(i)*feinheit);
-
-				Line linie = new Line();
-
-				linie.setStrokeWidth(feinheit*20);
-				linie.setStartX(startX);
-				linie.setStartY(startY);
-				linie.setEndX(endX);
-				linie.setEndY(endY);
-				root.getChildren().add(linie);
+			Scene scene = new Scene(root, 400, 400);
+			double dX = 0;
+			double dY = 0;
+			double xKor = 200;
+			double yKor = 200;
+			double radius = 30;
+			Path pfad = new Path();
+			pfad.getElements().add(new MoveTo(xKor + Math.sin(0) * radius, yKor + Math.cos(0) * radius));
+			for (double i = 0; i < 4 * 2 * Math.PI; i += 2 * Math.PI / 360) {
+				dX = Math.sin(i) * radius;
+				dY = Math.cos(i) * radius;
+				pfad.getElements().add(new LineTo(dX + xKor, dY + yKor));
+				radius += 0.1;
 			}
+			root.getChildren().add(pfad);
+			primaryStage.setScene(scene);
 			primaryStage.show();
-		} catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
